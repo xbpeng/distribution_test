fixing gradient bug

density_net.py
@@ -20,14 +20,10 @@ class DensityNet(object):
         h2 = U.relu(U.dense(h1, 32, weight_init=U.Xavier(1.0)))
         output = U.dense(h2, self.output_dim, weight_init=U.NormalizedColumns(1.0))
 
-        # hack
         net_params = tf.trainable_variables()
         grads = tf.gradients(output, net_params, -g_n)
         update_op = tf.train.AdamOptimizer(step_size).apply_gradients(zip(grads, net_params))
         
-        #loss = 0.5 * U.mean(U.square(g_n - output))
-        #update_op = tf.train.AdamOptimizer(step_size).minimize(loss)
-
         self.eval = U.function([x_n], output)
         self._step = U.function([x_n, g_n], update_op)
 
@@ -44,9 +40,7 @@ class DensityNet(object):
         return y
 
     def sample_xs(self, n):
-        # hack
-        #xs = np.array([np.random.randn(self.input_dim) for i in range(n)])
-        xs = np.array([np.random.rand(self.input_dim) * 2 - 1 for i in range(n)])
+        xs = np.array([np.random.randn(self.input_dim) for i in range(n)])
         return xs
 
     def update(self, x_n, g_n):

distribution_test.py
@@ -7,9 +7,8 @@ import density_net as dn
 def train(f, h, steps):
     batch_size = 32
     xs = h.sample_xs(batch_size)
-    # hack
-    #gs = f.eval_grad(xs)
-    gs = f.eval(xs) - h.eval(xs)
+    ys = h.eval(xs)
+    gs = f.eval_grad(ys)
     h.update(xs, gs)
 
 def main():
@@ -31,18 +30,13 @@ def main():
     i = 0
     while(True):
         train(f, h, iter_steps)
-        # hack
-        #samples = h.sample(num_samples)
-        #samples_flat = np.concatenate(samples)
+
+        samples = h.sample(num_samples)
+        samples_flat = np.concatenate(samples)
 
         plt.clf()
-        # hack
-        #plt.hist(samples_flat, num_bins, [x_min, x_max], normed=True, label='samples')
-        test_xs = np.reshape(xs, [-1, 1])
-        test_ys = h.eval(test_xs)
         plt.plot(xs, ys, 'g-', label='f(x)')
-        plt.plot(xs, test_ys, label='h(x)')
-        
+        plt.hist(samples_flat, num_bins, [x_min, x_max], normed=True, label='samples')
 
         axes = plt.gca()
         axes.set_xlim([x_min,x_max])