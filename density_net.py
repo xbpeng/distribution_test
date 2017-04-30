83f38a33a4a4a0a3d50d9b77058ed20dc3be8363
Jason <jasonpeng142@hotmail.com>
2017-04-30 11:36:36 -0700

    setting up nets

--- density_net.py
+++ density_net.py
@@ -1,15 +1,47 @@
 import numpy as np
 import sys
 import os
+import tensorflow as tf
 
 sys.path.append(os.path.abspath('../OpenAI_Gym'))
-import learning.tf_util as tf_util
+import learning.tf_util as U
 
 class DensityNet(object):
 
     def __init__(self):
+        self.input_dim = 1
+        self.output_dim = 1
+        step_size = 0.01
+
+        x_n = U.Input([None, self.input_dim], name='x')
+        g_n = U.Input([None, self.output_dim], name='g')
+
+        h1 = U.relu(U.dense(x_n, 64, weight_init=U.Xavier(1.0)))
+        output = U.dense(h1, self.output_dim, weight_init=U.NormalizedColumns(1.0))
+
+        net_params = tf.trainable_variables()
+
+        grads = tf.gradients(output, net_params, -g_n)
+        update_op = tf.train.AdamOptimizer(step_size).apply_gradients(zip(grads, net_params))
+
+        self._sample = U.function([x_n], output)
+        self._step = U.function([x_n, g_n], update_op)
+
+        U.initialize()
+
         return
 
     def sample(self):
-        x = np.random.normal(0, 0.5)
-        return x
+        return sample(self, 1)[0]
+
+    def sample(self, n):
+        xs = self.sample_xs(n)
+        y = self._sample(xs)
+        return y
+
+    def sample_xs(self, n):
+        xs = np.array([np.random.randn(self.input_dim) for i in range(n)])
+        return xs
+
+    def update(x_n, g_n):
+        self._step(x_n, g_n)
--- distribution_test.py
+++ distribution_test.py
@@ -5,13 +5,17 @@ import density_func as df
 import density_net as dn
 
 def train(f, h, steps):
+    batch_size = 32
+    xs = h.sample_xs(batch_size)
     return
 
 def main():
     x_min = -1
     x_max = 1
+    y_min = 0
+    y_max = 3
     dx = 0.01
-    num_samples = 1000
+    num_samples = 256
     num_bins = 20
     iter_steps = 1
 
@@ -21,16 +25,26 @@ def main():
 
     h = dn.DensityNet()
 
+    i = 0
+    while(True):
+        train(f, h, iter_steps)
+        samples = h.sample(num_samples)
+        samples_flat = np.concatenate(samples)
 
-    train(f, h, iter_steps)
-    samples = [h.sample() for i in range(num_samples)]
-    
-    plt.hist(samples, num_bins, [x_min, x_max], normed=True, label='samples')
-    plt.plot(xs, ys, 'g-', label='f(x)')
+        plt.clf()
+        plt.hist(samples_flat, num_bins, [x_min, x_max], normed=True, label='samples')
+        plt.plot(xs, ys, 'g-', label='f(x)')
 
-    plt.legend()
-    plt.xlabel('x')
-    plt.show()
+        axes = plt.gca()
+        axes.set_xlim([x_min,x_max])
+        axes.set_ylim([y_min,y_max])
+
+        plt.title('Iteration: ' + str(i))
+        plt.legend()
+        plt.xlabel('x')
+        plt.pause(0.05)
+
+        i += 1
 
 if __name__ == "__main__":
     main()
