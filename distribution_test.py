23dd5deb079ae0e4aee2aee539775909cd13c8af
Jason <jasonpeng142@hotmail.com>
2017-04-30 09:40:10 -0700

    setting up legend

--- distribution_test.py
+++ distribution_test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import density_func as df
 import density_net as dn
 
-def train(f, h):
+def train(f, h, steps):
     return
 
 def main():
@@ -13,20 +13,23 @@ def main():
     dx = 0.01
     num_samples = 1000
     num_bins = 20
+    iter_steps = 1
 
     f = df.DensityFunc()
     xs = np.arange(-1, 1, 0.01)
     ys = [f.eval(x) for x in xs]
 
     h = dn.DensityNet()
-    train(f, h)
 
+
+    train(f, h, iter_steps)
     samples = [h.sample() for i in range(num_samples)]
+    
+    plt.hist(samples, num_bins, [x_min, x_max], normed=True, label='samples')
+    plt.plot(xs, ys, 'g-', label='f(x)')
 
-    plt.plot(xs, ys)
+    plt.legend()
     plt.xlabel('x')
-    plt.hist(samples, num_bins, [x_min, x_max], normed=True)
-    plt.ylabel('f(x)')
     plt.show()
 
 if __name__ == "__main__":
