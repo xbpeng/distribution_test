0d9b3882eab1987f7c120552717cee94e2b8dfa1
Jason <jasonpeng142@hotmail.com>
2017-05-04 12:57:55 -0700

    color coding correspondences

--- distribution_test2d.py
+++ distribution_test2d.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 import density_func as df
@@ -78,6 +79,75 @@ def build_net():
     h = dn.DensityNet(input_dim, output_dim, step_size)
     return h
 
+def color_samples(samples, color_code):
+    num_samples = samples.shape[0]
+    cols = []
+    for i in range(num_samples):
+        if (color_code):
+            curr_sample = samples[i,:]
+
+            r = (curr_sample[0] + 2) * 0.2
+            g = 0
+            b = (curr_sample[1] + 2) * 0.2
+
+            r = np.maximum(0, np.minimum(r, 1))
+            g = np.maximum(0, np.minimum(g, 1))
+            b = np.maximum(0, np.minimum(b, 1))
+        else:
+            r = 0
+            g = 0
+            b = 1
+
+        c = [r, g, b]
+        cols.append(c)
+
+    return cols
+
+def plot_results(contour_X0, contour_X1, contour_Z, x_min, x_max, dx,
+                 sample_xs, samples, iter):
+    output_plots = False
+    enable_subplots = True
+    color_code = True
+    draw_correspondence = False
+
+    fig = plt.gcf()
+    plt.clf()
+        
+    if (enable_subplots):
+        plt.subplot(1, 2, 2)
+        
+    CS = plt.contour(contour_X0, contour_X1, contour_Z)
+    cols = color_samples(sample_xs, color_code)
+    plt.clabel(CS, inline=1, fontsize=10, label='f(x)')
+    plt.scatter(samples[:,0], samples[:,1], c=cols, label='samples', s=4, alpha=0.5)
+
+    axis0 = plt.gca()
+    axis0.set_xlim([x_min, x_max])
+    axis0.set_ylim([x_min, x_max])
+
+    plt.title('Iteration: ' + str(iter))
+    plt.legend()
+    axis0.set_aspect('equal', adjustable='box')
+
+    if (enable_subplots):
+        plt.subplot(1, 2, 1)
+        plt.scatter(sample_xs[:,0], sample_xs[:,1], c=cols, label='samples', s=4, alpha=0.5)
+        axis1 = plt.gca()
+        axis1.set_xlim([-3, 3])
+        axis1.set_ylim([-3, 3])
+        axis1.set_aspect('equal', adjustable='box')
+
+        if (draw_correspondence):
+            for p in range(0, samples.shape[0], 20):
+                con = matplotlib.patches.ConnectionPatch(xyA=sample_xs[p,:], xyB=samples[p,:], coordsA="data", coordsB="data",
+                                                        axesA=axis1, axesB=axis0, alpha=0.5)
+                axis1.add_artist(con)
+        
+    plt.pause(0.01)
+
+    if (output_plots):
+        plt.savefig('output/' + str(i).zfill(6) + '.png')
+
 def main():
     x_min = -2
     x_max = 2
@@ -107,21 +177,8 @@ def main():
 
         sample_xs = h.sample_xs(num_samples)
         samples = h.eval(sample_xs)
+        plot_results(X0, X1, Z, x_min, x_max, dx, sample_xs, samples, i)
 
-        plt.clf()
-        CS = plt.contour(X0, X1, Z)
-        plt.clabel(CS, inline=1, fontsize=10, label='f(x)')
-        plt.scatter(samples[:,0], samples[:,1], c='b', label='samples', s=4, alpha=0.5)
-
-        axes = plt.gca()
-        axes.set_xlim([x_min, x_max])
-        axes.set_ylim([x_min, x_max])
-
-        plt.title('Iteration: ' + str(i))
-        plt.legend()
-        plt.gca().set_aspect('equal', adjustable='box')
-        plt.pause(0.01)
-        #plt.savefig('output/' + str(i).zfill(6) + '.png')
 
 
 if __name__ == "__main__":
