eca8f398c6d3d205322b77da3e1118d6723924e6
Jason <jasonpeng142@hotmail.com>
2017-06-04 11:01:14 -0700

    tweaks

--- density_net.py
+++ density_net.py
@@ -19,12 +19,11 @@ class DensityNet(object):
 
         h1 = U.relu(U.dense(self.x_n, 64, weight_init=U.Xavier(1.0)))
         h2 = U.relu(U.dense(h1, 64, weight_init=U.Xavier(1.0)))
-        output = U.dense(h2, self.output_dim, weight_init=U.Xavier(1.0))
+        self._output = U.dense(h2, self.output_dim, weight_init=U.Xavier(1.0))
 
         net_params = tf.trainable_variables()
-        grads = tf.gradients(output, net_params, -self.g_n)
+        grads = tf.gradients(self._output, net_params, -self.g_n)
         
-        self.eval = U.function([self.x_n], output)
         self._step = tf.train.MomentumOptimizer(learning_rate=step_size, momentum=momentum).apply_gradients(zip(grads, net_params))
 
         U.initialize()
@@ -42,6 +41,12 @@ class DensityNet(object):
     def sample_xs(self, n):
         xs = np.array([np.random.randn(self.input_dim) for i in range(n)])
         return xs
+
+    def eval(self, x_n) :
+        feed = {
+            self.x_n : x_n
+        }
+        return self._output.eval(feed, session=U.SESSION)
 
     def update(self, x_n, g_n):
         feed = {
